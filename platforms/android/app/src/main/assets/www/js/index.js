4530cc2743c733b48089d34fd25f97aa2c92eb74
Chaha1n <chahaaaaan@gmail.com>
2020-06-17 20:11:14 +0900

    Changed button size

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,4 +1,10 @@
-function getAll() {
+document.getElementById("getAllButton").onclick = () => {
     //DBの任意のテーブルからデータを引っ張ってくる
-    alert("Hello!");
+    //show all data
+    document.getElementById("resultArea").innerHTML();
+}
+document.getElementById("execute_button").onclick = () => {
+    const sentence = document.getElementById("SQLBox").value;
+    //do sql sentence
+    document.getElementById("resultArea").innerHTML();
 }
